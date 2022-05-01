98460da4c0741ac9b7800761c9851b578a6a55d8
Владислав Соловейчик <soo.vlaad@gmail.com>
2022-05-01 21:31:07 +0300

    Добавлены urls и view-функции

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -5,5 +5,5 @@ from django.http import HttpResponse
 def index(request):    
     return HttpResponse('Главная страница')
 
-def group_posts(request):
+def group_posts(request, slug):
     return HttpResponse('Посты сообществ')
